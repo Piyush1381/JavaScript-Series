9bb090ba434924bab5706c856f267498814d1226
Piyush <Piyush24@gmail.com>
2024-01-11 11:28:16 +0530

    01-Variables

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,12 @@
-console.log("Piyush");
+const accountId = 24;
+let accountEmail = "piyush@gmail.com";
+var accountPassword = "12124";
+accountCity = "Mumbai";
+
+// accountId = 34;  //You can Not Change the value of constant variable
+
+accountEmail = "Pib@gmail.com";
+accountPassword = "24r4s5";
+accountCity = "Bangluru";
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
